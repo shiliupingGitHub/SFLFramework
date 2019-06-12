c8f9e6174cbf1e120a49dceb6bf3a460ec9e7171
shiliupingGithub <582441842@qq.com>
2019-06-12 17:32:13 +0800

    excel读取

--- Assets/GGame/Editor/ExcelImporterEditor.cs
+++ Assets/GGame/Editor/ExcelImporterEditor.cs
@@ -1,4 +1,5 @@
 
+using System.IO;
 using UnityEditor;
 
 namespace GGame.Editor
@@ -8,6 +9,20 @@ namespace GGame.Editor
         [MenuItem("Tools/Excel/Import")]
         static void ImportExcel()
         {
+            string path = "ConfigProject";
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path);
+
+                foreach (var file in files)
+                {
+                    var fileStream = File.OpenRead(file);
+
+                    var workbook = new NPOI.XSSF.UserModel.XSSFWorkbook(fileStream);
+                    
+                    fileStream.Dispose();
+                }
+            }
             
         }
         
