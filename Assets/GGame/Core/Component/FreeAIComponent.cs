32b8e9e14e784a9ca59ef2746d2ef6cf8eb8c1a7
shiliupingGithub <582441842@qq.com>
2019-06-12 15:55:43 +0800

    是的撒

--- Assets/GGame/Core/Component/FreeAIComponent.cs
+++ Assets/GGame/Core/Component/FreeAIComponent.cs
@@ -4,11 +4,23 @@ namespace GGame.Core
 {
     public class FreeAIComponent: Component
     {
+        public FreeMoveAIAgent Agent { get; set; }
         public override void Awake(World world, XmlNode node)
         {
             base.Awake(world, node);
-            int a = 0;
-            int b = a;
+
+            string btName = node.Attributes["name"].Value;
+            Agent = ObjectPool.Instance.Fetch<FreeMoveAIAgent>();
+            Agent.btload(btName);
+            Agent.btsetcurrent(btName);
+
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            ObjectPool.Instance.Recycle(Agent);
+            Agent = null;
         }
     }
 }
---
+++ Assets/GGame/Core/System/FreeAISystem.cs
@@ -0,0 +1,32 @@
+using NotImplementedException = System.NotImplementedException;
+
+namespace GGame.Core
+{
+    [Interest(typeof(FreeAIComponent))]
+    public class FreeAISystem : System
+    {
+        public override void OnUpdate()
+        {
+            
+        }
+
+        public override void OnTick()
+        {
+            foreach (FreeAIComponent component in _interestComponents)
+            {
+
+                if (null != component.Agent)
+                {
+                    behaviac.EBTStatus status = behaviac.EBTStatus.BT_RUNNING;
+                    
+                    while (status == behaviac.EBTStatus.BT_RUNNING)
+                    {
+                        
+                        status = component.Agent.btexec();
+                    }
+                }
+                
+            }
+        }
+    }
+}
