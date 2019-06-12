9b5dc037f522595419b2c2f7b2ad74cbbf4fd5e5
shiliupingGithub <582441842@qq.com>
2019-06-12 16:16:54 +0800

    我问问无

---
+++ Assets/GGame/Editor/ExcelImporterEditor.cs
@@ -0,0 +1,15 @@
+
+using UnityEditor;
+
+namespace GGame.Editor
+{
+    public class ExcelImporterEditor
+    {
+        [MenuItem("Tools/Excel/Import")]
+        static void ImportExcel()
+        {
+            
+        }
+        
+    }
+}
