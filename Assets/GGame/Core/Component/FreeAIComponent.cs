b3733d3d2fec5d34e42849f8c581b3074212862e
shiliupingGithub <582441842@qq.com>
2019-06-12 14:24:26 +0800

    阿萨德撒

---
+++ Assets/GGame/Core/Component/FreeAIComponent.cs
@@ -0,0 +1,14 @@
+using System.Xml;
+
+namespace GGame.Core
+{
+    public class FreeAIComponent: Component
+    {
+        public override void Awake(World world, XmlNode node)
+        {
+            base.Awake(world, node);
+            int a = 0;
+            int b = a;
+        }
+    }
+}
