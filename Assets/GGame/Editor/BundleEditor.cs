7b73fea0d298125f2fdcbfb58e8cd51966b740ef
shiliupingGithub <582441842@qq.com>
2019-06-12 13:16:21 +0800

    资源

--- Assets/GGame/Editor/BundleEditor.cs
+++ Assets/GGame/Editor/BundleEditor.cs
@@ -6,40 +6,45 @@ using UnityEditor;
 using UnityEditor.Build.Content;
 using UnityEditor.Build.Pipeline;
 using UnityEngine;
-using UnityEngine.Build.Pipeline;
 
-public class BundleEditor 
+namespace GGame.Editor
 {
-    [MenuItem("Assets/Bundle/BundleName")]
-    static void MakeBundleName()
-    {
-        foreach (var o in Selection.objects)
-        {
-            AssetImporter importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(o));
-
-            importer.assetBundleName = $"{o.name}.unity3d";
-
-            AssetDatabase.Refresh();
-        }
-    }
-
-    [MenuItem("Tools/Bundle/buildPC")]
-    static void BuildBundlePc()
-    {
-        var bundles = ContentBuildInterface.GenerateAssetBundleBuilds();
-        for (var i = 0; i < bundles.Length; i++)
-            bundles[i].addressableNames = bundles[i].assetNames.Select(Path.GetFileNameWithoutExtension).ToArray();
-        
-        var result = CompatibilityBuildPipeline.BuildAssetBundles($"{Application.streamingAssetsPath}/PC", BuildAssetBundleOptions.None,
-            BuildTarget.StandaloneWindows64);
-        var infoPath = Path.Combine(Application.streamingAssetsPath, "PC/Vertion.info");
-        var json = JsonUtility.ToJson(result);
-        
-        File.WriteAllText(infoPath, json);
-        AssetDatabase.Refresh();
-        
-        
-        
-    }
     
+    public class BundleEditor 
+    {
+        [MenuItem("Assets/Bundle/BundleName")]
+        static void MakeBundleName()
+        {
+            foreach (var o in Selection.objects)
+            {
+                AssetImporter importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(o));
+
+                importer.assetBundleName = $"{o.name}.unity3d";
+
+                AssetDatabase.Refresh();
+            }
+        }
+
+        [MenuItem("Tools/Bundle/buildPC")]
+        static void BuildBundlePc()
+        {
+            var bundles = ContentBuildInterface.GenerateAssetBundleBuilds();
+            for (var i = 0; i < bundles.Length; i++)
+                bundles[i].addressableNames = bundles[i].assetNames.Select(Path.GetFileNameWithoutExtension).ToArray();
+        
+            var result = CompatibilityBuildPipeline.BuildAssetBundles($"{Application.streamingAssetsPath}/PC", BuildAssetBundleOptions.None,
+                BuildTarget.StandaloneWindows64);
+            var infoPath = Path.Combine(Application.streamingAssetsPath, "PC/Vertion.info");
+            var json = JsonUtility.ToJson(result);
+        
+            File.WriteAllText(infoPath, json);
+            AssetDatabase.Refresh();
+        
+        
+        
+        }
+    
+    }
 }
+
+
---
+++ Assets/GGame/Editor/ResAssetPostprocessor.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using UnityEditor;
+
+namespace GGame.Editor
+{
+    public class ResAssetPostprocessor : AssetPostprocessor
+    {
+
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths)
+        {
+            foreach (var asset in importedAssets)
+            {
+                string assetName = Path.GetFileNameWithoutExtension(asset);
+
+                if (asset.Contains("Assets/GGame/Res"))
+                {
+                    AssetImporter importer = AssetImporter.GetAtPath(asset);
+
+                    importer.assetBundleName = $"{assetName}.unity3d";
+                }
+                
+               
+            }
+        }
+    }
+}
